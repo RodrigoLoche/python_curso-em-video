d9050a827bde3f0be0512489c6967dab5e942b8b
Rodrigo Loche <rodrigoloche@gmail.com>
2025-12-23 12:04:13 +0900

    Create aula_2.py

---
+++ aula_2.py
@@ -0,0 +1,15 @@
+# Tipos Primitivos
+
+# int, float, bool, str
+
+n1 = input("Digite um valor: ") # str
+print(type(n1))
+
+n1 = int(input("Digite um valor: ")) # int
+print(type(n1))
+
+n1 = float(input("Digite um valor: ")) # float
+print(type(n1))
+
+n1 = input("Digite algo: ") #bool
+print(n1.isnumeric())
