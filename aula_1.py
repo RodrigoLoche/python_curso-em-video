82251687eb5d7190efa379e88969a6d38cf190a3
Rodrigo Loche <rodrigoloche@gmail.com>
2025-12-23 12:03:35 +0900

    Create aula_1.py

---
+++ aula_1.py
@@ -0,0 +1,17 @@
+print("Olá Mundo!") # Imprime imagem na tela.
+print(7 + 3) #Soma
+print("7" + "4") # Junta
+
+nome = "Jubileu"
+idade = 30
+peso = 70.5
+
+print(nome, idade, peso) # Imprime cada variável.
+
+
+# O input pede informação ao usuário.
+nome = input("Qual é o seu nome? ")
+idade = input("Qual é a sua idade? ")
+peso = input("Qual é o seu peso? ")
+
+print(nome, idade, peso)
